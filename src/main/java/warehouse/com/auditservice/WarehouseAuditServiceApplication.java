c9fc807b90c83a53cd3cd3480b9fe817f4053529
Vladyslav Shakhno <vshakhno@lenovo.com>
2023-10-03 18:11:48 +0300

    01 - initial commit

---
+++ src/main/java/warehouse/com/auditservice/WarehouseAuditServiceApplication.java
@@ -0,0 +1,13 @@
+package warehouse.com.auditservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseAuditServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WarehouseAuditServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/warehouse/com/auditservice/WarehouseAuditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package warehouse.com.auditservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseAuditServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
